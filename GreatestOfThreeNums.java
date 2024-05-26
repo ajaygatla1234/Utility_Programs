ad4a9f96d21f54329167b1d72aa3a37fae521049
ajaygatla1234 <149674849+ajaygatla1234@users.noreply.github.com>
2024-05-26 21:31:15 +0530

    Add files via upload

---
+++ GreatestOfThreeNums.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.util.Scanner;
+
+public class GreatestOfThreeNums {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Taking input for three numbers
+		System.out.print("Enter the first number: ");
+		int num1 = scanner.nextInt();
+
+		System.out.print("Enter the second number: ");
+		int num2 = scanner.nextInt();
+
+		System.out.print("Enter the third number: ");
+		int num3 = scanner.nextInt();
+
+		// Finding the greatest number
+		int greatest = 0;
+
+		if (num1 == num2 && num1 == num3) {
+			System.out.println("All three numbers are equal.");
+		}
+		else {
+			if (num1 >= num2 && num1 >= num3) {
+				greatest = num1;
+			} else if (num2 >= num1 && num2 >= num3) {
+				greatest = num2;
+			} else {
+				greatest = num3;
+			}
+
+			// Printing the greatest number
+			System.out.println("The greatest number is: " + greatest);
+		}
+		scanner.close();
+	}
+}
